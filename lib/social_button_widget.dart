0ee198b99d42db49eef4584a9622240b70bd887c
Lelio Palmisano <lpjr03@gmail.com>
2025-04-24 18:32:32 +0200

    new text field and social button widgets

---
+++ lib/social_button_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'text_widget.dart';
+
+class SocialButtonWidget extends StatelessWidget {
+  final Color bgColor;
+  final String imagePath;
+  final VoidCallback onPress;
+
+  const SocialButtonWidget(
+      {super.key, required this.bgColor, required this.imagePath, required this.onPress});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+          padding: const EdgeInsets.all(8),
+          height: 40,
+          width: 120,
+          decoration: BoxDecoration(
+            color: bgColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image(
+                image: AssetImage(imagePath),
+              ),
+              TextWidget(
+                title: "Google",
+                txtSize: 18.0,
+                txtColor: Colors.black,
+              ),
+            ],
+          )),
+    );
+  }
+}
