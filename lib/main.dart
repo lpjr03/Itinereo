6df38cf736ae05da45b2a0f64e6aa6397c4d26ec
Lelio Palmisano <lpjr03@gmail.com>
2025-04-24 18:53:07 +0200

    1st version of login, sign up and forget password screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import '/welcome_screen.dart'; 
+import 'package:itinereo/login_screen.dart';
+import 'package:itinereo/signup_screen.dart';
+import 'welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Itinereo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: const WelcomeScreen(),
+      title: 'Itinereo',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFFBFCFF),
+        scaffoldBackgroundColor: const Color(0xFF262b31),
+        secondaryHeaderColor: const Color(0xFF151a1d),
+      ),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+      },
+      home: Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: ((context, snapshot) {
+            if (snapshot.hasData) {
+              return const WelcomeScreen();//mettere HomePage()
+            } else {
+              return const WelcomeScreen();
+            }
+          }),
+        ),
+      ),
     );
   }
 }
