c5256d33cc8e3942a207f5ce077895d3a425b92a
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-04-28 21:22:18 +0200

    Modified social-button widget

--- lib/login_manager/social_button_widget.dart
+++ lib/login_manager/social_button_widget.dart
@@ -6,33 +6,37 @@ class SocialButtonWidget extends StatelessWidget {
   final String imagePath;
   final VoidCallback onPress;
 
-  const SocialButtonWidget(
-      {super.key, required this.bgColor, required this.imagePath, required this.onPress});
+  const SocialButtonWidget({
+    super.key,
+    required this.bgColor,
+    required this.imagePath,
+    required this.onPress,
+  });
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPress,
       child: Container(
-          padding: const EdgeInsets.all(8),
-          height: 40,
-          width: 120,
-          decoration: BoxDecoration(
-            color: bgColor,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Image(
-                image: AssetImage(imagePath),
-              ),
-              TextWidget(
-                title: "Google",
-                txtSize: 18.0,
-                txtColor: Colors.black,
-              ),
-            ],
-          )),
+        padding: const EdgeInsets.all(8),
+        height: 40,
+        width: 120,
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image(image: AssetImage(imagePath)),
+            TextWidget(
+              title:
+                  "Google", //@todo pass the social name as parameter in order to generalize
+              txtSize: 18.0,
+              txtColor: Colors.black,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
