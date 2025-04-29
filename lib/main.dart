fbe6afed1f8b580e601e7275b372a0781c60c17f
Lelio Palmisano <lpjr03@gmail.com>
2025-04-29 11:27:07 +0200

    TEMP: Skip login/signup and launch directly for testing purposes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:itinereo/login_manager/log_in_checker.dart';
+import 'package:itinereo/itinereo_manager.dart';
 
-void main() async {
+/**
+ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const LogInChecker());
-}
+}**/
 
+void main() async{
+  runApp(const ItinereoManager());
+}
