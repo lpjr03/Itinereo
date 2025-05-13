bffb4cc0b5a4f4dd6e5ca41d9e84498ac566cc00
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-05-13 14:04:39 +0200

    Fix Log in Validator

--- lib/login_manager/validator.dart
+++ lib/login_manager/validator.dart
@@ -1,8 +1,13 @@
 String? Function(String?)? emailValidator = (String? value) {
   if (value == null || value.isEmpty) {
-    return 'Please Enter Email';
-  } else if (!value.contains('@')) {
-    return 'Please Enter Valid Email';
+    return 'Please enter an email';
+  }
+  final emailRegex = RegExp(
+    r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+  );
+
+  if (!emailRegex.hasMatch(value)) {
+    return 'Please enter a valid email';
   }
   return null;
 };
