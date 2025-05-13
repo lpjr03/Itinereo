2d4e9216c6d92f54d913c430f9a25de51d51b1ed
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-05-13 16:22:55 +0200

    Updated lifting up state for Camera screen

--- lib/itinereo_manager.dart
+++ lib/itinereo_manager.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:itinereo/add_diary_page.dart';
+import 'package:itinereo/camera_screen.dart';
 import 'package:itinereo/diary_preview.dart';
 import 'package:itinereo/diary_screen.dart';
 import 'package:itinereo/home_screen.dart';
@@ -30,6 +34,7 @@ class ItinereoManager extends StatefulWidget {
 class _ItinereoState extends State<ItinereoManager> {
   /// Stores the identifier of the currently active screen.
   var activeScreen = 'home-screen';
+  CameraDescription? selectedCamera;
 
   /// Switches the active screen to the diary screen.
   void switchToDiary() {
@@ -52,16 +57,33 @@ class _ItinereoState extends State<ItinereoManager> {
     });
   }
 
+  /// Switches the active screen to the camera screen.
+  void switchToCameraScreen() async {
+    final cameras = await availableCameras();
+    setState(() {
+      selectedCamera = cameras.first;
+      activeScreen = 'camera-screen';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // Selects the widget to display based on the active screen.
     Widget screenWidget = HomeScreen(switchToDiary);
     if (activeScreen == 'diary-screen') {
-      screenWidget = DiaryScreen(switchToPreview: switchToEntriesPreview, switchToAddDiaryPage: switchToAddDiaryPage,);
+      screenWidget = DiaryScreen(
+        switchToPreview: switchToEntriesPreview,
+        switchToAddDiaryPage: switchToAddDiaryPage,
+      );
     } else if (activeScreen == 'preview-screen') {
       screenWidget = const DiaryPreview();
-    }else if(activeScreen == 'add-diary-page-screen'){
-      screenWidget = AddDiaryEntryPage(onSave: switchToEntriesPreview);
+    } else if (activeScreen == 'add-diary-page-screen') {
+      screenWidget = AddDiaryEntryPage(
+        onSave: switchToEntriesPreview,
+        switchToCameraScreen: switchToCameraScreen,
+      );
+    } else if (activeScreen == 'camera-screen') {
+      screenWidget = CameraScreen(camera: selectedCamera, onBack: switchToAddDiaryPage,);
     }
 
     return MaterialApp(
