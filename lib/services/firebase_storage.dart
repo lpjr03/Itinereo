90bea766324faceac0726126326eee2703038ed1
Lelio Palmisano <lpjr03@gmail.com>
2025-05-14 12:03:32 +0200

    add new method to delete photo in firebase storage

--- lib/services/firebase_storage.dart
+++ lib/services/firebase_storage.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:itinereo/exceptions/photo_exceptions.dart';
 
 class StorageService {
   final _storage = FirebaseStorage.instance;
@@ -13,4 +14,14 @@ class StorageService {
     final uploadTask = await ref.putFile(imageFile);
     return await uploadTask.ref.getDownloadURL();
   }
+
+  Future<void> deletePhoto(String downloadUrl) async {
+  try {
+    final ref = _storage.refFromURL(downloadUrl);
+    await ref.delete();
+  } catch (e) {
+    throw PhotoDeleteException('Impossibile eliminare la foto: $e');
+  }
+}
+
 }
