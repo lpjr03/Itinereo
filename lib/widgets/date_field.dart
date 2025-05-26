4964be291d80607ec9cf70076f508358d6bb1d30
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-05-26 12:21:14 +0200

    Create dateField widget

---
+++ lib/widgets/date_field.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class DateField extends StatefulWidget {
+  const DateField({super.key});
+
+  @override
+  State<DateField> createState() => _DateFieldState();
+}
+
+class _DateFieldState extends State<DateField> {
+  final TextEditingController _dateController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _dateController.text = _formatDate(DateTime.now());
+  }
+
+  String _formatDate(DateTime date) {
+    return '${date.day}/${date.month}/${date.year}';
+  }
+
+  Future<void> _pickDate() async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        _dateController.text = _formatDate(picked);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _dateController,
+      readOnly: true,
+      onTap: _pickDate,
+      style: const TextStyle(fontStyle: FontStyle.italic),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: const Color(0xFFF6ECD4),
+        hintText: 'Select date',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(color: Color(0xFFD8CCB1), width: 2),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(color: Color(0xFFD8CCB1), width: 2),
+        ),
+      ),
+    );
+  }
+}
