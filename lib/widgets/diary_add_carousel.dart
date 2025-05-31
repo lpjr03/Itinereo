7d5bf2870d5f5114be7a8b4e8a473522c406a7cf
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-05-31 11:36:48 +0200

    Update photocarousel widget with Listview

--- lib/widgets/diary_add_carousel.dart
+++ lib/widgets/diary_add_carousel.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:itinereo/widgets/polaroid_photo.dart';
 
 class DiaryPhotoCarousel extends StatelessWidget {
   final List<String> photoUrls;
   final VoidCallback onAddPhoto;
-  final void Function(int index) onRemovePhoto;
+  final void Function(String photoPath) onRemovePhoto;
   final double height;
 
   const DiaryPhotoCarousel({
@@ -14,96 +14,104 @@ class DiaryPhotoCarousel extends StatelessWidget {
     required this.photoUrls,
     required this.onAddPhoto,
     required this.onRemovePhoto,
-    this.height = 250,
+    this.height = 320,
   });
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(
-        height: height,
-        enableInfiniteScroll: false,
-        viewportFraction: 1.0,
-        enlargeCenterPage: true,
-        scrollPhysics: const BouncingScrollPhysics(),
-      ),
-      items: List.generate(photoUrls.length < 5 ? photoUrls.length + 1 : 5, (
-        index,
-      ) {
-        final isAddCard = index == photoUrls.length && photoUrls.length < 5;
+    return SizedBox(
+      height: height,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        physics: PageScrollPhysics(),
 
-        if (isAddCard) {
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-            decoration: BoxDecoration(
-              color: const Color(0xFFFFF2D8),
-              border: Border.all(color: Color(0xFFD8CCB1), width: 2),
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Center(
-              child: IconButton(
-                onPressed: onAddPhoto,
-                icon: const Icon(
-                  Icons.add_a_photo,
-                  size: 48,
-                  color: Color(0xFF2E5355),
-                ),
-                splashRadius: 32,
-                tooltip: 'Add a new Photo',
+        padding: EdgeInsets.symmetric(vertical: 10),
+        itemCount: photoUrls.length < 5 ? photoUrls.length + 1 : 5,
+        itemBuilder: (context, index) {
+          final isAddCard = index == photoUrls.length && photoUrls.length < 5;
+
+          if (isAddCard) {
+            return Container(
+              width: 325,
+              margin: EdgeInsets.symmetric(horizontal: 4),
+
+              decoration: BoxDecoration(
+                color: const Color(0xFFFFF2D8),
+                border: Border.all(color: Colors.white, width: 20),
               ),
-            ),
-          );
-        }
+              child: Center(
+                child: IconButton(
+                  onPressed: onAddPhoto,
+                  icon: const Icon(
+                    Icons.add_a_photo,
+                    size: 90,
+                    color: Color(0xFF2E5355),
+                  ),
+                  splashRadius: 32,
+                  tooltip: 'Add a new Photo',
+                ),
+              ),
+            );
+          }
 
-        final photoUrl = photoUrls[index];
-        return Stack(
-          children: [
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder:
-                      (_) => Dialog(
-                        backgroundColor: Colors.black,
-                        insetPadding: const EdgeInsets.all(16),
-                        child: InteractiveViewer(
-                          child: Image.file(File(photoUrl)),
+          final photoUrl = photoUrls[index];
+          return Stack(
+            children: [
+              FilledButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder:
+                        (_) => Dialog(
+                          backgroundColor: Colors.black,
+                          insetPadding: const EdgeInsets.all(16),
+                          child: InteractiveViewer(
+                            child: Image.file(File(photoUrl)),
+                          ),
                         ),
-                      ),
-                );
-              },
-              child: Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 10,
+                  );
+                },
+                style: FilledButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 4),
+                  backgroundColor: Colors.transparent,
+                  shadowColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
                 ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  image: DecorationImage(
-                    image:  FileImage(File(photoUrl)) as ImageProvider,
-                    fit: BoxFit.cover,
+                child: PolaroidPhoto(
+                  imagePath: photoUrl,
+                  backgroundColor: const Color(0xFFFFF2D8),
+                  caption: 'Photo ${index + 1}/5',
+                  width: 325,
+                ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.close_rounded,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    onRemovePhoto(photoUrls[index]);
+                  },
+                  tooltip: 'Remove photo',
+                  padding: EdgeInsets.zero,
+                  constraints: BoxConstraints(),
+                  splashRadius: 20,
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(Colors.grey),
+                    shape: WidgetStateProperty.all(CircleBorder()),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              top: 6,
-              right: 6,
-              child: GestureDetector(
-                onTap: () => onRemovePhoto(index),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.black54,
-                    shape: BoxShape.circle,
-                  ),
-                  padding: const EdgeInsets.all(4),
-                  child: const Icon(Icons.close, color: Colors.white, size: 16),
-                ),
-              ),
-            ),
-          ],
-        );
-      }),
+            ],
+          );
+        },
+      ),
     );
   }
 }
