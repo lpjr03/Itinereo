8976f055ff14dc23d7f399b3a90c1b961d5514ac
Lelio Palmisano <lpjr03@gmail.com>
2025-06-05 00:26:05 +0200

    add flutter_dotenv dependency and load .env file in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:itinereo/login_manager/log_in_checker.dart';
-import 'package:itinereo/itinereo_manager.dart';
 
  void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseAppCheck.instance.activate(
