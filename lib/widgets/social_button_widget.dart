b7c0444208ab9828cfcfb2a61e08d59a3ba10155
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-06-06 16:13:34 +0200

    Remove unused import

--- lib/widgets/social_button_widget.dart
+++ lib/widgets/social_button_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'text_widget.dart';
 
 class SocialButtonWidget extends StatelessWidget {
   final Color bgColor;
