cdd4072dbc3576d15b037f8ade4437c7fc7eefcc
Lelio Palmisano <lpjr03@gmail.com>
2025-06-08 17:09:38 +0200

    remove firestore local caching

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:itinereo/login_manager/log_in_checker.dart';
 
- void main() async {
+void main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await FirebaseAppCheck.instance.activate(
-  androidProvider: AndroidProvider.debug,
-);
+    androidProvider: AndroidProvider.debug,
+  );
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: false,
+  );
   runApp(const LogInChecker());
 }
