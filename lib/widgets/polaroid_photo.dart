10cf1159170aa72d02f45ba17f297df269a5a013
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-06-08 20:04:55 +0200

    Remove Captation form PolaroidPhoto

--- lib/widgets/polaroid_photo.dart
+++ lib/widgets/polaroid_photo.dart
@@ -1,16 +1,15 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class PolaroidPhoto extends StatelessWidget {
-  final String? imagePath; 
-  final Widget? content; 
+  final String? imagePath;
+  final Widget? content;
   final Color backgroundColor;
   final double angle;
   final bool isAsset;
-  final String? caption;
   final double? width;
   final Widget? trailingAction;
+  final bool showShadow;
 
   const PolaroidPhoto({
     super.key,
@@ -19,9 +18,9 @@ class PolaroidPhoto extends StatelessWidget {
     required this.backgroundColor,
     this.angle = 0,
     this.isAsset = false,
-    this.caption,
     this.width,
     this.trailingAction,
+    this.showShadow = true,
   }) : assert(
          imagePath != null || content != null,
          'You must provide an image or content',
@@ -49,64 +48,19 @@ class PolaroidPhoto extends StatelessWidget {
       mainContent = Image(image: imageProvider, fit: BoxFit.cover);
     }
 
-    final borderWidth = caption == null ? 10.0 : 15.0;
-
     return Transform.rotate(
       angle: angle,
       child: Container(
         width: w,
         decoration: BoxDecoration(
           color: backgroundColor,
-          boxShadow: const [BoxShadow(color: Colors.black26, blurRadius: 8)],
-          border:
-              caption == null
-                  ? Border.all(color: backgroundColor, width: borderWidth)
-                  : Border(
-                    top: BorderSide(color: backgroundColor, width: borderWidth),
-                    left: BorderSide(
-                      color: backgroundColor,
-                      width: borderWidth,
-                    ),
-                    right: BorderSide(
-                      color: backgroundColor,
-                      width: borderWidth,
-                    ),
-                    bottom: BorderSide(
-                      color: backgroundColor,
-                      width: borderWidth,
-                    ),
-                  ),
+          boxShadow:
+              showShadow
+                  ? const [BoxShadow(color: Colors.black26, blurRadius: 8)]
+                  : [],
+          border: Border.all(color: backgroundColor, width: 10),
         ),
-        child:
-            caption == null
-                ? AspectRatio(aspectRatio: 1, child: mainContent)
-                : Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    AspectRatio(aspectRatio: 4 / 3, child: mainContent),
-                    Container(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              caption!,
-                              style: GoogleFonts.courierPrime(
-                                fontSize: 14,
-                                color: Colors.black87,
-                              ),
-                              textAlign: TextAlign.start,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          if (trailingAction != null) trailingAction!,
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+        child: AspectRatio(aspectRatio: 1, child: mainContent),
       ),
     );
   }
