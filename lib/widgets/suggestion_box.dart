3a8ce446012488630b13cb912a8eeb134083da54
SaraFelix03 <168423152+SaraFelix03@users.noreply.github.com>
2025-06-11 10:55:52 +0200

    refactor: enhance documentation for SuggestedItinerariesBox widget

--- lib/widgets/suggestion_box.dart
+++ lib/widgets/suggestion_box.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/// A styled container that displays a list of suggested itineraries.
+///
+/// - Displays a section title
+/// - Renders a vertical list of itinerary card widgets
+///
+/// The list of [cards] can include custom widgets such as itinerary previews.
 class SuggestedItinerariesBox extends StatelessWidget {
+  /// The list of widgets to display as suggested itineraries.
+  ///
+  /// Typically each item is a custom card widget like [ItineraryCard].
   final List<Widget> cards;
 
+  /// Creates a [SuggestedItinerariesBox] with the provided list of [cards].
   const SuggestedItinerariesBox({super.key, required this.cards});
 
   @override
@@ -39,10 +49,11 @@ class SuggestedItinerariesBox extends StatelessWidget {
             itemCount: cards.length,
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 6),
-              child: cards[index],
-            ),
+            itemBuilder:
+                (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: cards[index],
+                ),
           ),
         ],
       ),
